f1799e229c0d82666cd094042f13f8e0a7c28b7d
Abdulfattah Safa <fattah.safa@gmail.com>
2023-06-04 15:04:12 +0300

    modified:   src/london_baseline.py

--- Part_1_finetune/src/london_baseline.py
+++ Part_1_finetune/src/london_baseline.py
@@ -2,3 +2,9 @@
 #   example in the dev set.
 # Hint: Make use of existing code.
 # Your solution here should only be a few lines.
+import utils
+place = ['London']
+total, correct = utils.evaluate_places('birth_dev.tsv', place * len([line for line in open('birth_dev.tsv').read().split('\n') if line]))
+
+if total > 0:
+    print('Correct: {} out of {}: {}%'.format(correct, total, correct/total*100))
